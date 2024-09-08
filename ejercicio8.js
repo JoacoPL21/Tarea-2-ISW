8b2fce2734a6af1d0525ba76dee67b79acb245ea
JoacoPL1 <joaquin.perez2101@alumnos.ubiobio.cl>
2024-09-08 15:13:20 -0300

    new file:   ejercicio7.js 	new file:   ejercicio8.js

---
+++ ejercicio8.js
@@ -0,0 +1,24 @@
+const persona={
+nombre: 'Joaquin',
+edad: 22,
+genero: 'masculino'
+}
+
+console.log(persona);
+
+const caja={
+    cuaderno:'torre',
+    lapiz:'mina 2B',
+    fotografias: 5,
+    estado: true
+}
+console.log(caja);
+console.log(caja.cuaderno);
+console.log(caja.lapiz);
+console.log(caja.fotografias);
+console.log(caja.estado);
+console.log(typeof caja);
+console.log(typeof caja.cuaderno);
+console.log(typeof caja.lapiz);
+console.log(typeof caja.fotografias);
+console.log(typeof caja.estado);
